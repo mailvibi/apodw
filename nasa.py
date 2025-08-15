5c8c203fff84352259fc4ff243e6d4090f06b746
Vibi Sreenivasan <mail.vibi@gmail.com>
2025-08-15 19:52:19 +0200

    use key from nasa if available as env

--- nasa.py
+++ nasa.py
@@ -1,12 +1,15 @@
 import requests
+import os
 
-APOD_KEY="DEMO_KEY"
-APOD_URL="https://api.nasa.gov/planetary/apod?api_key=" + APOD_KEY
 
 def get_apod():
     K_HDURL="hdurl"
     K_EXPLANATION="explanation"
     K_TITLE="title"
+    NASA_KEY = os.getenv("NASA_KEY")
+    APOD_KEY="DEMO_KEY" if not NASA_KEY else NASA_KEY
+    APOD_URL="https://api.nasa.gov/planetary/apod?api_key=" + APOD_KEY
+
     r = requests.get(APOD_URL)
     r.raise_for_status()
     jres = r.json()
